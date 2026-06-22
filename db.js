8ed853f8ddb180b684777f4d29c9500804c0fc1f
sparsh-kapil08 <sparshkapil2008@gmail.com>
2026-06-22 17:09:27 +0530

    changes

--- db.js
+++ db.js
@@ -7,10 +7,10 @@ const db1=new Pool({
     database:process.env.DATABASE,
     user:process.env.USER,
     password:process.env.PASS,
-    ssl: process.env.NODE_ENV === 'production' ? {
-        rejectUnauthorized: false,
-        sslmode: 'require'
-    } : false,
+    family: 4,
+    ssl: {
+        rejectUnauthorized: false
+    },
     statement_timeout: 30000,
     query_timeout: 30000,
     max: 20,
